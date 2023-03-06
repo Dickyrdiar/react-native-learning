c1f090c271db15026bf9a477b1801b517150d644
dicky ardiar <diackyardiar1@gmail.com>
2023-03-06 15:20:27 +0700

    try login with github

--- src/redux/fetching.ts
+++ src/redux/fetching.ts
@@ -1,18 +1,25 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import axios from 'axios';
+// import {config} from 'process';
 import {ParsingProps} from '../lib/TypeData/cardMenu.type';
 import {TagProps} from '../lib/TypeData/tagCarousel.types';
+import {Octokit} from '@octokit/core';
+import {useAuthProps} from '../lib/TypeData/userAuthProps';
 
 interface myState {
   data: ParsingProps[];
   tagList: TagProps[];
+  token: string | null;
   isLoading: boolean;
   error: string | null | undefined;
+  user?: useAuthProps[];
 }
 
 const initialState: myState = {
   data: [],
   tagList: [],
+  user: [],
+  token: null,
   isLoading: true,
   error: null,
 } as myState;
@@ -27,10 +34,38 @@ export const fetchTaglist = createAsyncThunk('tags/fetchTags', async () => {
   return responseTag.data;
 });
 
+export const authenticateUser = createAsyncThunk(
+  'user/authentication',
+  async (code: string): Promise<string> => {
+    const responseLogin = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      {
+        client_id: YOUR_CLIENT_ID,
+        client_secret: YOUR_CLIENT_SECRET,
+        code,
+      },
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    return responseLogin.data.access_token;
+  },
+);
+
 export const dataSlice = createSlice({
   name: 'data',
   initialState,
-  reducers: {},
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: state => {
+      state.user = undefined;
+    },
+  },
   extraReducers: builder => {
     // state for data
     builder.addCase(fetchData.pending, state => {
